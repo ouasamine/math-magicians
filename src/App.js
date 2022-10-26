84b47a863a7865d2303cc7e1c2db05ef06596889
Amine <ouas.amine@outlook.com>
2022-10-26 11:34:00 +0100

    Convert App component to class component in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
